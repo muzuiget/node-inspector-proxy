27f3cdca723b71826df481078a7e7e3105335cbd
muzuiget <muzuiget@gmail.com>
2018-03-30 17:13:33 +0800

    change url

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const server = new ProxyServer();
 console.info(`
 Debugger Proxy listening on port ${port}.
 To start debugging, open the following URL in Chrome:
-    chrome-devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&ws=127.0.0.1:${port}/9229
+    chrome-devtools://devtools/bundled/node_app.html?ws=127.0.0.1:${port}/9229
 The suffix of the URL "9229" is the port which you want to proxy.
 `);
 server.listen(port, host);
