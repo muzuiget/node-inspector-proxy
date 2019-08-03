ec62fb821f75cefd70e0bc3c4d41aa200aecc0b5
muzuiget <muzuiget@gmail.com>
2019-08-03 13:22:39 +0800

    merge code to single file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import WebSocket from 'ws';
 import http from 'http';
 import liburl from 'url';
 
-export default class ProxyServer {
+class ProxyServer {
 
     constructor() {
         this.server = null;
@@ -120,3 +120,16 @@ export default class ProxyServer {
     }
 
 }
+
+const address = process.argv[2] || '0.0.0.0:9230';
+const [host, port] = address.split(':');
+const server = new ProxyServer();
+console.info(`
+Debugger Proxy listening on port ${port}.
+To start debugging, open the following URL in Chrome:
+
+    chrome-devtools://devtools/bundled/js_app.html?ws=127.0.0.1:${port}/9229
+
+The suffix of the URL "9229" is the port which you want to proxy.
+`);
+server.listen(port, host);
